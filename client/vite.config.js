f509a3185f32d322dc93b02221b2ca93f4b37863
Sumanthkalluri0128 <163962409+Sumanthkalluri0128@users.noreply.github.com>
2026-09-25 12:18:39 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: ["VITE_", "API_"],
+
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env.API_URL": JSON.stringify(env.API_URL),
+    },
+  };
 });
