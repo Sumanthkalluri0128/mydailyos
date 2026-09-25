0196d37731d7e718450363ebb5db794bdc6b2218
Sumanthkalluri0128 <163962409+Sumanthkalluri0128@users.noreply.github.com>
2026-09-25 11:30:35 +0530

    Delete server directory

--- server/models/Activity.js
+++
@@ -1,2 +0,0 @@
-const mongoose=require('mongoose');
-const s=new mongoose.Schema({name:{type:String,required:true,trim:true},category:{type:String,required:true,trim:true},met:{type:Number,required:true,min:0},description:{type:String,default:''},isFavorite:{type:Boolean,default:false}},{timestamps:true});module.exports=mongoose.model('Activity',s);
--- server/models/WeightLog.js
+++
@@ -1,2 +0,0 @@
-const mongoose=require('mongoose');
-module.exports=mongoose.model('WeightLog',new mongoose.Schema({userId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true,index:true},date:{type:String,required:true},weightKg:{type:Number,required:true,min:1},notes:{type:String,default:''}},{timestamps:true}));
