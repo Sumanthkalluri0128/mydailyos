7e7bf7109a5a1985b42e71c519a0314a23f329a1
Sumanthkalluri0128 <163962409+Sumanthkalluri0128@users.noreply.github.com>
2026-09-25 12:23:48 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 
-const rawApiUrl ='https://mydailyos.onrender.com';
+const rawApiUrl =import.meta.env.API_URL || 'https://mydailyos.onrender.com';
 
 export const API_URL = rawApiUrl.replace(/\/+$/, '');
 export const API_URL = API_API_URL;
